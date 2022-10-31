787e9ea558f180b4119a706fe2a4f1bbf4bfb28b
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-10-31 21:18:41 +0800

    分析ref属性

---
+++ vue_cli/02_src_ref属性/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1 v-text="msg" ref="title"></h1>
+        <button @click="showDOM" ref="btn">点我输出上方的DOM元素</button>
+        <School ref="sch"></School>
+        
+    </div>
+</template>
+
+<script>
+    import School from './components/School.vue'
+    export default {
+        name:'App',
+        components:{
+            School
+        },
+        data() {
+            return {
+                msg:'欢迎学习'
+            }
+        },
+        methods: {
+            showDOM() {
+                // console.log(this);  // 指向VueComponent
+                console.log(this.$refs);
+                console.log(this.$refs.btn);  // 获取真实DOM元素
+                console.log(this.$refs.title);  // 获取真实DOM元素
+                console.log(this.$refs.sch); //可以是school组件加refs属性 获得的是组件事例对象vc
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
