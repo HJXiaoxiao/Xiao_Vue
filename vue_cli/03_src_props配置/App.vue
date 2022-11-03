3e0a2fef12ffb0112cd70d8cdce037308ac43b7a
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-11-03 21:29:17 +0800

    分析props属性

---
+++ vue_cli/03_src_props配置/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- v-bind 双线绑定，传数值型18 -->
+        <Student name="李四" sex="女" :age="18"></Student>    
+        <!-- <Student name="李四" sex="女" ></Student>     -->
+    </div>
+</template>
+
+<script>
+    import Student from './components/Student.vue'
+    export default {
+        name:'App',
+        components:{
+            Student
+        },
+        data() {
+            return {
+                todos: [
+                    {id: '001', title: '吃饭', done: false},
+                    {id: '002', title: "睡觉", done: true},
+                    {id: '003', title: '打代码', done: false}
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vue_cli/03_src_props配置/components/Student.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <h2>学校姓名:{{name}}</h2>
+        <h2>学校性别:{{sex}}</h2>
+        <h2>学校年龄:{{myAge}}</h2>
+        <button @click="updateAge">尝试修改年龄</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Student',
+        data() {
+            return {
+                msg:'我是一个Guet的学生',
+                myAge:this.age   // 优先接收props中的age
+            }
+        },
+        methods: {
+            updateAge() {
+                // 不建议修改外部传入的数据
+                // this.age = 22;  
+                // 若要修改外部数据应如下修改
+                this.myAge++;
+            }
+        },
+        // 1.简单接收
+        // props:['name', 'sex', 'age']  
+
+        // 2.接收的同时对数据类型进行限制
+/*         props:{
+            name:String,
+            sex:String,
+            age:Number
+        } */
+        // 3.接收的同时对数据类型进行限制 + 默认值的指令 + 必要性的限制
+        props:{
+            name:{
+                type:String,    // 类型
+                require:true    // require 必须
+            },
+            age:{
+                type: Number,
+                default: 99     //默认值
+            },
+            sex:{
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
