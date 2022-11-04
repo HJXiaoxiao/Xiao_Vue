3b3ec400be8d61a7d3526e77312a2d9b6317aa37
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-11-04 15:25:23 +0800

    scoped样式

---
+++ vue_cli/06_src_scoped样式/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <School/>
+        <hr/>
+        <Student/>
+    </div>
+</template>
+
+<script>
+    // 若style中类名相冲突，最后引入的组件样式会覆盖之前的组件样式
+    import School from './components/School.vue'    
+    import Student from './components/Student.vue'
+    
+    export default {
+        name:'App',
+        components:{
+            Student,
+            School
+        }
+    }
+</script>
+
+<style>
+
+</style>
