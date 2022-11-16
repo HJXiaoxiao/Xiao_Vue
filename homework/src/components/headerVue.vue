fb308d530c8a0b787a0d469fae61053e61debe32
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-11-16 09:52:18 +0800

    一般组件

---
+++ homework/src/components/headerVue.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="line"></div>
+          
+        <el-menu
+            :default-active="$route.path" 
+            class="el-menu-demo"
+            mode="horizontal"
+            background-color="#545c64"
+            text-color="#fff"
+            router
+            @select="handleSelect"
+            active-text-color="#ffd04b">
+                <el-menu-item index="/">首页</el-menu-item>
+                <el-menu-item index="/ziliao">资料库</el-menu-item>
+                <el-menu-item index="/shuju">监测数据</el-menu-item>
+                <el-menu-item index="/zhaoping">人员招聘</el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'headerVue',
+        data() {
+            return {
+                activeIndex: '1',
+                activeIndex2: '/'
+            };
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+</script>
+
+<style>
+    a {
+        text-decoration: none;
+    }
+
+</style>
