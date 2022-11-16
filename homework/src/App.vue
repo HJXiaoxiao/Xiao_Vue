296f1611a984bbe18d44f0308f898e7b61876a12
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-11-16 09:53:16 +0800

    vue文件

---
+++ homework/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-header>
+      <headerVue/>
+    </el-header>
+   
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+    <!-- <el-footer>
+      <footerVue/>
+    </el-footer> -->
+  </div>
+</template>
+
+<script>
+  import headerVue from './components/headerVue'
+
+  export default {
+    name: 'App',
+    components: {
+      headerVue,
+    }
+  }
+</script>
+
+<style>
+
+</style>
