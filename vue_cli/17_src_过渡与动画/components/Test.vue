a5157488c48cdb0c0e5975cec4f63c470b245ce9
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-11-22 09:55:10 +0800

    vue中的过渡与动画

---
+++ vue_cli/17_src_过渡与动画/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <!-- appear一加载就有动画  :appear="true"    简写 appear -->
+        <transition name="hello" :appear="true">
+            <h1 v-show="isShow">你好啊！</h1>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Test',
+        data() {
+            return {
+                isShow:true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    h1 {
+        background-color: orange;
+    }
+
+/*     .v-enter-active {
+        animation: guet 0.5s linear;
+    }
+    .v-leave-active {
+        animation: guet 0.5s reverse linear;
+    } */
+    /* 若动画transition有name属性，则.v-enter-active =》 .(name)-enter-active */
+    .hello-enter-active {
+        animation: guet 0.5s linear;
+    }
+    .hello-leave-active {
+        animation: guet 0.5s reverse linear;
+    }
+
+    @keyframes guet {
+        from {
+            transform: translateX(-100%);
+        }
+        to {
+            transform: translateX(0px);
+        }
+    }
+</style>
---
+++ vue_cli/17_src_过渡与动画/components/Test3.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>   
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <!-- appear一加载就有动画  :appear="true"    简写 appear -->
+        <transition-group 
+            name="animate__animated animate__bounce" 
+            enter-active-class="animate__swing"
+            leave-active-class="animate__backOutUp"
+            >
+            <h1 v-show="!isShow" key="1">你好啊！</h1>
+            <h1 v-show="isShow" key="2">GUET</h1>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+    // 引入css样式：import + 路径
+    import 'animate.css'
+    export default {
+        name:'Test3',
+        data() {
+            return {
+                isShow:true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    h1 {
+        background-color: orange;
+    }
+</style>
