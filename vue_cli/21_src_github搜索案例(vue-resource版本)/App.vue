14f9bb353b85aed3e5c4fb239d7a8146c0354405
HJXiaoxiao <94539548+HJXiaoxiao@users.noreply.github.com>
2022-11-26 22:23:54 +0800

    github搜索案例(vue-resource发送请求)

---
+++ vue_cli/21_src_github搜索案例(vue-resource版本)/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <Search></Search>
+    <List></List>
+  </div>
+</template>
+
+<script>
+    import Search from './components/Search.vue'
+    import List from './components/List.vue'
+
+    export default {
+      name:'App',
+      components: { 
+        Search:Search,
+        List:List
+      },
+       
+    }
+</script>
+
+<style>
+
+</style>
